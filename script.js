198fdf0d7b210dc5edafd19575e91835d8774e3d
TurxanR2024 <166982283+TurxanR2024@users.noreply.github.com>
2024-04-14 11:29:11 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+var result = addNumbers(5, 3);
+console.log(result); 
+
+
+function subtractNumbers(a, b) {
+    return a - b;
+}
+
+var result = subtractNumbers(10, 3);
+console.log(result); //  7
+
+
+
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+var result = multiplyNumbers(5, 4);
+console.log(result); //  20
+
+
